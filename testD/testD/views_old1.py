be5db6aa2134d9c7a9a6ef72f10a5c1ca5486deb
hadi2f244 <hadi2f244@ymail.com>
2014-04-21 19:10:32 +0430

    first commit

---
+++ testD/testD/views_old1.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.contrib import auth
+from django.core.context_processors import csrf 
+from django.contrib.auth.forms import UserCreationForm
+
+#account registeration
+
+def register_user(request):
+	if request.method =="POST":	
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect('/accounts/register_success')
+
+	args={}
+	args.update(csrf(request))
+	args['form'] = UserCreationForm()
+	return render_to_response('register.html',args)
+
+def register_success(request):
+	#if request.REFERER=='http://127.0.0.1:8000/accounts/register/':
+	#	print "ok"
+	return render_to_response('register_success.html')
+
+#account login views
+def login(request):
+	#print request.user.username
+	if len(request.user.username) is 0:
+		c = {}
+		c.update(csrf(request))
+		return render_to_response('login.html' , c)
+	else:
+		return HttpResponseRedirect('/accounts/loggedin')
+	
+
+def auth_view(request):
+	username=request.POST.get('username','')
+	password=request.POST.get('password','')
+	user=auth.authenticate(username=username,password=password) 
+	
+	if user is not None:
+		auth.login(request,user)
+		return HttpResponseRedirect('/accounts/loggedin')
+	else:
+		return HttpResponseRedirect('/accounts/invalid_login')
+
+def loggedin(request):
+	if len(request.user.username) is not 0:
+		return render_to_response('loggedin.html',
+								{'full_name':request.user.username})
+	else:
+		return HttpResponseRedirect('/accounts/login')
+
+def invalid_login(request):
+	if len(request.user.username) is 0:
+		return render_to_response('invalid_login.html')
+	else:
+		return HttpResponseRedirect('/accounts/loggedin')
+
+def logout(request):
+	auth.logout(request)
+	return render_to_response('logout.html')
