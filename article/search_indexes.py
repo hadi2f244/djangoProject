40ac56ee56859438f36bc7704c2fecb67608416b
alireza <sarsanaee@gmail.com>
2014-05-25 23:58:36 +0430

    clearing search index

--- article/search_indexes.py
+++ article/search_indexes.py
@@ -1,6 +1,6 @@
 import datetime
 from haystack import indexes
-from models import Article,Comment
+from models import Article
 
 class ArticleIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
