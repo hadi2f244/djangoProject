a8226509fe6b6a292803ec7d1d0fb572af15863f
Alireza <sarsanaee@gmail.com>
2014-08-24 16:51:30 +0430

    adding ckeditor to admin

--- new/admin.py
+++ new/admin.py
@@ -1,4 +1,21 @@
 from django.contrib import admin
 from new.models import New
-admin.site.register(New)
+from ckeditor.widgets import CKEditorWidget
+from django import forms
+
+
+class NewAdminForm(forms.ModelForm):
+    body = forms.CharField(widget=CKEditorWidget())
+
+    class Meta:
+        model = New
+
+
+class NewAdmin(admin.ModelAdmin):
+    form = NewAdminForm
+
+
+admin.site.register(New, NewAdmin)
+
+
 
