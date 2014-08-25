4f6956b037d2f1ff1b528e0d7ffcf06628cfb88b
hadi2f244 <hadi2f244@ymail.com>
2014-08-25 17:42:14 +0430

    allowHosts avazi

--- mainProject/settings.py
+++ mainProject/settings.py
@@ -30,7 +30,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
 #there is just a test
 #another test
 # Local time zone for this installation. Choices can be found here:
